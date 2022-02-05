a1ea4c469ae036236bc0f724661c26c1f5f6154b
Nasreddine <eln@outlook.be>
2022-02-05 02:55:43 +0100

    setup nx workspace

---
+++ apps/tacos-mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tacos.dev',
+  appName: 'Tacos',
+  webDir: '../../dist/apps/tacos-mobile',
+  bundledWebRuntime: false
+};
+
+export default config;
