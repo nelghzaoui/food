7ffbfe93c062d8cf214dba6d2f999bf663055c9b
Nasreddine <eln@outlook.be>
2022-02-26 16:05:36 +0100

    update package json

--- apps/tacos-mobile/capacitor.config.ts
+++ apps/tacos-mobile/capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tacos.dev',
-  appName: 'Tacos',
-  webDir: '../../dist/apps/tacos-mobile',
+  appId: 'be.tacos.txnas',
+  appName: 'tacos',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
