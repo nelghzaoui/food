ce50df504860905c072661822112faf6e1098aa3
Nasreddine <eln@outlook.be>
2022-03-19 01:21:34 +0100

    update order module

--- apps/tacos-mobile/src/app/features/tabs/order/views/order.page.ts
+++ apps/tacos-mobile/src/app/features/tabs/order/views/order.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { SegmentCustomEvent } from '@ionic/angular';
+import { NavController, SegmentCustomEvent } from '@ionic/angular';
 import { OrderFlow } from '../models/order-flow.enum';
 import { OrderService } from '../services/order.service';
 import { OrderQuery } from '../redux/order.query';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './order.page.html',
@@ -12,11 +13,17 @@ export class OrderPage implements OnInit {
   OrderFlow = OrderFlow;
   currentFlow$ = this.orderQuery.currentFlow$;
 
-  constructor(private readonly orderService: OrderService, private readonly orderQuery: OrderQuery) {}
+  constructor(
+    private readonly navCtrl: NavController,
+    private readonly route: ActivatedRoute,
+    private readonly orderService: OrderService,
+    private readonly orderQuery: OrderQuery
+  ) {}
 
   ngOnInit(): void {}
 
   onSegmentChange(event: Event) {
+    this.navCtrl.navigateForward([(event as SegmentCustomEvent).detail.value], { relativeTo: this.route });
     this.orderService.update({ currentFlow: (event as SegmentCustomEvent).detail.value as OrderFlow });
   }
 }
