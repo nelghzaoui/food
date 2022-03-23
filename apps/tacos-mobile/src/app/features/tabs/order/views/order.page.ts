0e5a43dd5130192d8ca313f5697ab54c03cda760
Nasreddine <eln@outlook.be>
2022-03-23 01:29:59 +0100

    update order page

--- apps/tacos-mobile/src/app/features/tabs/order/views/order.page.ts
+++ apps/tacos-mobile/src/app/features/tabs/order/views/order.page.ts
@@ -1,25 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { OrderFlow, orderFlows } from '../models/order-flow.enum';
-import { OrderQuery } from '../redux/order.query';
 
 @Component({
   templateUrl: './order.page.html',
   styleUrls: ['./order.page.scss'],
   selector: 'food-order-page'
 })
-export class OrderPage implements OnInit {
+export class OrderPage {
   OrderFlow = OrderFlow;
   flows = orderFlows;
 
-  constructor(
-    private readonly navCtrl: NavController,
-    private readonly route: ActivatedRoute,
-    private readonly orderQuery: OrderQuery
-  ) {}
-
-  ngOnInit(): void {}
+  constructor(private readonly navCtrl: NavController, private readonly route: ActivatedRoute) {}
 
   onSelect(flow: OrderFlow) {
     this.navCtrl.navigateForward([flow], { relativeTo: this.route });
