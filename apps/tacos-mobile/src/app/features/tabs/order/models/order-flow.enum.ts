285ff2601dbb6379c2a3ee36fb13039420dc86d2
Nasreddine <eln@outlook.be>
2022-03-23 01:22:16 +0100

    add salad

--- apps/tacos-mobile/src/app/features/tabs/order/models/order-flow.enum.ts
+++ apps/tacos-mobile/src/app/features/tabs/order/models/order-flow.enum.ts
@@ -3,5 +3,15 @@ export enum OrderFlow {
   TACOS = 'tacos',
   DRINK = 'drink',
   FINGER = 'finger',
-  DESSERT = 'dessert'
+  DESSERT = 'dessert',
+  SALAD = 'salad'
 }
+
+export const orderFlows = [
+  OrderFlow.MENU,
+  OrderFlow.TACOS,
+  OrderFlow.FINGER,
+  OrderFlow.DRINK,
+  OrderFlow.DESSERT,
+  OrderFlow.SALAD
+];
