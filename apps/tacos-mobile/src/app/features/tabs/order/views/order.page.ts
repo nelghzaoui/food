2012eddd4240666eb26f1ff8f08c2db5891840d8
Nasreddine <eln@outlook.be>
2022-03-23 01:22:04 +0100

    update order page

--- apps/tacos-mobile/src/app/features/tabs/order/views/order.page.ts
+++ apps/tacos-mobile/src/app/features/tabs/order/views/order.page.ts
@@ -1,29 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, SegmentCustomEvent } from '@ionic/angular';
-import { OrderFlow } from '../models/order-flow.enum';
-import { OrderService } from '../services/order.service';
-import { OrderQuery } from '../redux/order.query';
+import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { OrderFlow, orderFlows } from '../models/order-flow.enum';
+import { OrderQuery } from '../redux/order.query';
 
 @Component({
   templateUrl: './order.page.html',
-  styleUrls: ['./order.page.scss']
+  styleUrls: ['./order.page.scss'],
+  selector: 'food-order-page'
 })
 export class OrderPage implements OnInit {
   OrderFlow = OrderFlow;
-  currentFlow$ = this.orderQuery.currentFlow$;
+  flows = orderFlows;
 
   constructor(
     private readonly navCtrl: NavController,
     private readonly route: ActivatedRoute,
-    private readonly orderService: OrderService,
     private readonly orderQuery: OrderQuery
   ) {}
 
   ngOnInit(): void {}
 
-  onSegmentChange(event: Event) {
-    this.navCtrl.navigateForward([(event as SegmentCustomEvent).detail.value], { relativeTo: this.route });
-    this.orderService.update({ currentFlow: (event as SegmentCustomEvent).detail.value as OrderFlow });
+  onSelect(flow: OrderFlow) {
+    this.navCtrl.navigateForward([flow], { relativeTo: this.route });
   }
 }
