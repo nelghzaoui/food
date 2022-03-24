efe0dd9b354c266dde4a38a25f9fe61a146e76fb
Nasreddine <eln@outlook.be>
2022-03-24 19:02:57 +0100

    fetch all foods at the start of the app

--- apps/tacos-mobile/src/app/core/services/food/food.service.ts
+++ apps/tacos-mobile/src/app/core/services/food/food.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { FoodCategory } from '@food/shared/models/food';
-import { BehaviorSubject, map, mapTo, take } from 'rxjs';
+import { take } from 'rxjs';
+import { CoreStore } from '../../redux/core.store';
 import { ApiService } from '../api/api.service';
 import { MobileFood } from './models/mobile-food.interface';
 
@@ -8,27 +8,12 @@ import { MobileFood } from './models/mobile-food.interface';
 export class FoodService {
   private readonly FOOD_API = 'food';
 
-  private readonly foodsSubject$ = new BehaviorSubject<MobileFood[]>([]);
-  foods$ = this.foodsSubject$.asObservable();
+  constructor(private readonly apiService: ApiService, private readonly coreStore: CoreStore) {}
 
-  constructor(private readonly apiService: ApiService) {
-    this.getAllFoods();
-  }
-
-  private getAllFoods() {
-    this.apiService
+  init() {
+    return this.apiService
       .get<MobileFood[]>(this.FOOD_API)
       .pipe(take(1))
-      .subscribe((foods: MobileFood[]) => this.foodsSubject$.next(foods));
-  }
-
-  getTacos() {
-    this.apiService
-      .get<MobileFood[]>(this.FOOD_API)
-      .pipe(
-        take(1),
-        map((foods) => foods.filter((r) => r.category === FoodCategory.TACOS))
-      )
-      .subscribe((tacos: MobileFood[]): void => console.log(tacos));
+      .subscribe((foods: MobileFood[]) => this.coreStore.update({ foods }));
   }
 }
